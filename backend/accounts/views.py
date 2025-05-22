c3e5a7934b5317727b899d398a36393467c05947
Shivam Likhar <shivam1likhar14209211@gmail.com>
2025-05-23 00:32:40 +0530

    Backend Add

---
+++ backend/accounts/views.py
@@ -0,0 +1,30 @@
+from rest_framework import generics, status
+from rest_framework.response import Response
+from .serializers import RegisterSerializer
+from rest_framework.permissions import AllowAny
+
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({"message": "User created successfully"}, status=status.HTTP_201_CREATED)
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['username'] = user.username
+        return token
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
