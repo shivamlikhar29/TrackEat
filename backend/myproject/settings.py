4d62606433bf7cb88d048c13495918243f7c8086
Shivam Likhar <shivam1likhar14209211@gmail.com>
2025-05-23 14:18:46 +0530

    Cors error

--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -25,7 +25,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware'
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -40,6 +40,7 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "https://trackeat-auch.onrender.com",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
